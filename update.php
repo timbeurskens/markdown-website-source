7882abd8313868edbbda21380e6747e2c431171a
timbeurskens <timbeurskens@keybase.io>
2016-04-07 16:58:37 +0200

    Simplify updater script & add security

--- update.php
+++ update.php
@@ -15,6 +15,7 @@ if(!file_exists("../options.json")){
 	throw new Exception("options.json: file not found at " . realpath("../options.json"), 1);
 	die();
 }
+
 $optionsRaw = file_get_contents("../options.json");
 $options = $default_options;
 try {
@@ -25,59 +26,79 @@ try {
 $tempLocation = $options['temp_location'];
 $archiveLocation = $tempLocation . "release.tar.gz";
 $releaseFileLocation = $options['working_dir'];
-if(isset($_POST['payload'])){
+
+$secretHash = "sha1=" . sha1($options['secret']);
+
+if(!isset($_SERVER['HTTP_X_HUB_SIGNATURE']) || $_SERVER['HTTP_X_HUB_SIGNATURE'] != $secretHash){
+	throw new Exception("Not a valid signature!", 1);
+	die();
+}
+
+if(!isset($_POST['payload'])){
+	throw new Exception("No payload available!", 1);
+	die();
+}
+
+$payload = array();
+
+try{
 	$payload = json_decode($_POST['payload'], true);
-	if(isset($payload['action']) && $payload['action'] == "published" && isset($payload['release']) && isset($payload['repository'])){
-		$repo = $payload['repository'];
-		$release_match = "/" . str_replace("/", "-", $repo['full_name']) . ".*/";
-		$release = $payload['release'];
-		if(isset($release['tarball_url']) && $release['tarball_url'] != ""){
-			$tarball_url = $release['tarball_url'];
-			//download file
-			try{
-				//set_time_limit(0); // unlimited max execution time
-				$dest_file = fopen($archiveLocation, 'wb');
-				$options = array(
-				  CURLOPT_FILE    => $dest_file,
-				  CURLOPT_TIMEOUT => 28800, // set this to 8 hours so we dont timeout on big files
-				  CURLOPT_URL     => $tarball_url,
-				  CURLOPT_USERAGENT => "markdown-update-downloader",
-				  CURLOPT_FOLLOWLOCATION => true
-				);
+} catch(Exception $e){
+	die("Error decoding payload");
+}
 
-				$ch = curl_init();
-				curl_setopt_array($ch, $options);
-				curl_exec($ch);
-				curl_close($ch);
-				fclose($dest_file);
+if(!isset($payload['release']) || !isset($payload['repository'])){
+	throw new Exception("Payload does not contain the right information", 1);
+	die();
+}
 
-				if(!(filesize($archiveLocation) > 0)){
-					throw new Exception("Empty file!", 1);
-				}
+$repo = $payload['repository'];
+$release_match = "/" . str_replace("/", "-", $repo['full_name']) . ".*/"; // match name-repo-version regex
+$release = $payload['release'];
+$tarball_url = $release['tarball_url'];
 
-				$phar = new PharData($archiveLocation);
-				$phar->extractTo($tempLocation, null, true);
-				unset($phar);
-				Phar::unlinkArchive($archiveLocation);
-				$dirs = scandir($tempLocation);
-				removeDir($releaseFileLocation);
-				foreach($dirs as $dir){
-					if(preg_match($release_match, $dir) == 1){
-						rename($tempLocation . $dir, $releaseFileLocation);
-					}
-				}
-				setDirectoryPermissions($releaseFileLocation, 0644, 0755);
-			} catch(Exception $e){
-				print_r($e);
-			}
-		}else{
-			echo "No tarball url detected";
-		}
-	}else{
-		echo "Not a valid request.";
-		print_r($payload);
+try {
+	$dest_file = fopen($archiveLocation, 'wb');
+	$options = array(
+		CURLOPT_FILE    => $dest_file,
+		CURLOPT_TIMEOUT => 28800, // set this to 8 hours so we dont timeout on big files
+		CURLOPT_URL     => $tarball_url,
+		CURLOPT_USERAGENT => "markdown-update-downloader",
+		CURLOPT_FOLLOWLOCATION => true
+	);
+
+	$ch = curl_init();
+	curl_setopt_array($ch, $options);
+	curl_exec($ch);
+	curl_close($ch);
+	fclose($dest_file);
+
+	if(!(filesize($archiveLocation) > 0)){
+		throw new Exception("Empty file!", 1);
 	}
-}else{
-	echo "no payload";
+} catch (Exception $e) {
+	die("Error downloading file: " . print_r($e, true));
+}
+
+try {
+	$phar = new PharData($archiveLocation);
+	$phar->extractTo($tempLocation, null, true);
+	unset($phar);
+	Phar::unlinkArchive($archiveLocation);
+} catch (Exception $e) {
+	die("Error extracting archive: " . print_r($e, true));
+}
+
+try {
+	$dirs = scandir($tempLocation);
+	removeDir($releaseFileLocation);
+	foreach($dirs as $dir){
+		if(preg_match($release_match, $dir) == 1){
+			rename($tempLocation . $dir, $releaseFileLocation);
+		}
+	}
+	setDirectoryPermissions($releaseFileLocation, 0644, 0755);
+} catch (Exception $e) {
+	die("Error moving files or changing permissions: " . print_r($e, true));
 }
 ?>
